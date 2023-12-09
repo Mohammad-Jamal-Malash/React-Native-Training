8baca669853be56a870cc345739a1992dc85ab80
unknown <m.abomalash@student.aaup.edu>
2023-12-09 12:09:07 +0200

    Learning navigation This is the app init

---
+++ NavApp/App.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>App hi my name is mohammad jamal </Text>
+    </View>
+  )
+}
+
+export default App
