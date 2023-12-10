c4dcc5fa07b18b9181f8db53de28272d86ff3cae
unknown <m.abomalash@student.aaup.edu>
2023-12-10 23:35:44 +0200

    Add the navigation to the 'Home Screen' Vid.49

--- NavApp/src/screens/Home.tsx
+++ NavApp/src/screens/Home.tsx
@@ -1,10 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const Home = () => {
+
+import {RootStackParamList} from '../App'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+
+//this also belong to the type saftiy shit
+type HomeProps = NativeStackScreenProps<RootStackParamList,'Home'>
+
+const Home = ({navigation}:HomeProps) => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style = {styles.container}>
+      <Text style = {styles.smallText}>Home Screen</Text>
+
+      <Button title='Go Details' 
+      // onPress={() =>{
+      //   navigation.navigate("Details" ,{ productID:"86"})
+      // }}
+
+      // Another way using 'push'
+      onPress={()=> navigation.push('Details',{productID:'86'})}
+      />
+
     </View>
   )
 }
