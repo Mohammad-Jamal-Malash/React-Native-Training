757be22e36b2e1649be998145400b8830f50b446
unknown <m.abomalash@student.aaup.edu>
2023-12-11 15:30:21 +0200

    add the separator & productItem vid.53

---
+++ PhoneStore/src/components/Separator.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Separator = () => {
+  return (
+    <View style = {styles.Separator}>
+      
+    </View>
+  )
+}
+
+export default Separator
+
+const styles = StyleSheet.create({
+    Separator:{
+        height:0.8,
+        color:'#CAD5E2'
+    }
+})
